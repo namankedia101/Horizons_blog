db91f30cc26b82e2a4e9687951b7ec2ea0810c83
Your Name <you@example.com>
2021-08-29 21:51:40 +0530

    First Commit- Made a Blog App

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://warm-brushlands-22534.herokuapp.com/' });
 //const url = "http://localhost:5000/posts/api";
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
