9765f2749446cf9eec1f418c32473277e62c98ac
Your Name <you@example.com>
2021-09-11 02:10:42 +0530

    Added EMAIL VERIFICATION [about,comments,similarPosts,newsletter] left

--- controllers/user.js
+++ controllers/user.js
@@ -28,7 +28,7 @@ const sendMail =async(email,code)=>{
             clientId:process.env.CLIENT_ID,
             clientSecret:process.env.CLIENT_SECRET,
             refreshToken:process.env.REFRESH_TOKEN,
-            accessToken
+            accessToken:accessToken
         }
     });
 
